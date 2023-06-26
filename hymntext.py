d981a70dcee0df337cc2921ab25fb397157e13d6
Nerstage <mirandadcwu@gmail.com>
2023-06-26 16:14:26 -0700

    basic hymn text reading

---
+++ hymntext.py
@@ -0,0 +1,41 @@
+class Hymn:
+    def __init__(self, title: str, file: str) -> None:
+        self.title = title
+        self.file = file
+        self.create_verses()
+
+    def get_num_verses(self):
+        lines = read_hymn(self.file)
+        return lines.count("") + 1
+
+    def create_verses(self):
+        lines = read_hymn(self.file)
+        self.verses = [""]
+        curr_verse = 0
+        for line in lines:
+            if line == "":
+                curr_verse = curr_verse + 1
+                self.verses.append("")
+            else:
+                if self.verses[curr_verse] == "":
+                    self.verses[curr_verse] = line
+                else:
+                    self.verses[curr_verse] = self.verses[curr_verse] + "\n" + line
+
+
+def read_hymn(file: str) -> list:
+    lines = []
+    with open(file, "r") as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+def main():
+    test = Hymn("O for a Thousand", "57.txt")
+    print(test.title)
+    print(test.verses)
+
+
+if __name__ == "__main__":
+    main()
